d3bcfac4908ec1148a1a4fc34e07521d5d92d858
AbdullahAzza1 <abdullahazza141@gmail.com>
2023-11-22 16:14:11 +0100

    step8

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
+            conversazioneAttiva: 1,
             contacts: [
                 {
                     name: 'Michele',
